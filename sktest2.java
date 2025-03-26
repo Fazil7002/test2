c233957754bac00602a3e9665e5f74fe026d9b47
madhuk54 <apoorvav2006@gmail.com>
2025-03-26 12:51:15 +0530

    pushing the file

---
+++ sktest2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class content {
+    public static void main (String[] args){
+        String science = ["data Analysis","Programming"];
+        String math = ["Matrix","Geometry"];
+        String figma = ["User Interface"];
+        System.out.print("Prefer Your Content");
+    }
+    if science==1{
+        System.out.print("content:"+Science);
+    }
+    else if math == 2{
+        System.out.print("content:"+math);
+    }
+    else if figma == 3 {
+        System.out.print("content:"+figma)
+    }
+}
